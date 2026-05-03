a17c5bb7f65e73e93f0b6f0da55562092c41c621
Aleksandar Taragoshev <taragoshevaleksandar@gmail.com>
2026-05-03 17:25:07 +0100

    Add: non-overlapping-intervals - submission-6

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-6.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+
+        vector<vector<int>> res;
+
+        sort(intervals.begin(), intervals.end());
+
+        res.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            // DEBUG:
+            // cout << "(" <<interval[0] << ", " << interval[1] << ")";
+            
+            int lastEnd = res.back()[1];
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+
+            if ( currStart >= lastEnd ) { // Check if not overlapping
+                res.push_back(intervals[i]); // Add interval to new array
+            } 
+            else if (currEnd <= lastEnd) {
+                // Check if another element has an end time that is before the previous endtime
+                // This way we find an earlier (smaller) end time
+                // Less chance of overlap with a following interval
+                // Minimize # of intervals that need to be deleted 
+                res.back() = intervals[i];
+            }
+        }
+
+        // DEBUG:
+        // for (auto& i : res) {
+        //     cout << "(" <<i[0] << ", " << i[1] << ")";
+        // }
+
+        // Find diff between original array and non-overlapping array
+        int minRemoval = intervals.size() - res.size();
+        return minRemoval;
+    }
+};
